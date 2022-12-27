9e4619ed0e421ae7ca50af5ab28dc6a1791bab4a
kkaoww <120167981+kkaoww@users.noreply.github.com>
2022-12-27 16:34:56 +0700

    Update lab10_2.cpp

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,7 +1,24 @@
 #include<iostream>
-using namespace std;
+using namespace std ;
 
-//Write the function printO() here
+void printO(int N, int M)
+{
+    if (N > 0 && M > 0)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            for (int i = 0; i < M; i++)
+            {
+            cout << "O";
+            }
+        cout << endl;
+    }
+}
+    else
+    {
+    cout << "Invalid input";
+}
+}
 
 int main(){
 	
